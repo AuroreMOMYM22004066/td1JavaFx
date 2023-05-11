d8362c3b6a249628b0a62ac08b8697c6f5e15317
m22004066 <au.momy2004@gmail.com>
2023-05-11 17:31:04 +0200

    fin de séance

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,9 @@ module com.example.td1javafx {
 
     opens com.example.td1javafx to javafx.fxml;
     exports com.example.td1javafx;
+    exports com.example.partie1_1;
+    exports com.example.partie1_2;
+    exports com.example.partie1_3;
+    exports com.example.partie2_1;
+
 }
