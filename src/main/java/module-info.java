518a0b4d4e2a316d77abd009c81870f7fd0d4ee2
m22004066 <au.momy2004@gmail.com>
2023-05-15 11:56:11 +0200

    test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,5 +9,8 @@ module com.example.td1javafx {
     exports com.example.partie1_2;
     exports com.example.partie1_3;
     exports com.example.partie2_1;
+    exports com.example.partie2_2;
+    opens com.example.partie3 to javafx.fxml;
+    exports com.example.partie3;
 
 }
